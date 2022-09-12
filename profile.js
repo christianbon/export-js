b06e88bc1ea31d96a861999b30fd3814689632b9
christianbon <bonafena@alterra.id>
2022-09-12 10:56:53 +0700

    add mapping data

--- profile.js
+++ profile.js
@@ -46,9 +46,12 @@ function getSelfData() {
     document.getElementById("profile-role").innerHTML = res.client_profile?.userPosition ? res.client_profile?.userPosition : '-';
     document.getElementById("field-department").innerHTML = res.client_profile?.department ? res.client_profile?.department : '-';
 
+    const industryList =  res.client_profile?.industry_types.length > 0 ?  res.client_profile?.industry_types.map((data)=>{
+      return data.name
+    }) : []
     document.getElementById("profile-company-name").innerHTML = res.client_profile?.companyName ? res.client_profile?.companyName : '-';
     document.getElementById("profile-about").innerHTML = res.client_profile?.about ? res.client_profile?.about : '-';
-    document.getElementById("profile-industry").innerHTML = res.client_profile?.industry_types.length > 0 ? res.client_profile?.industry_types.toString() : '-';
+    document.getElementById("profile-industry").innerHTML = res.client_profile?.industry_types.length > 0 ? industryList.toString() : '-';
     document.getElementById("profile-address").innerHTML = res.client_profile?.address ? res.client_profile?.address : '-';
     document.getElementById("profile-website").innerHTML = res.client_profile?.companyWebsite ? res.client_profile?.companyWebsite : '-';
     document.getElementById("profile-instagram").innerHTML = res.client_profile?.instagram ? res.client_profile?.instagram : '-';
